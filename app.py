9083805125fc71be24725e8febfff8ca7a392f0c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 19:38:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.title("My website")
+
+with st.sidebar:
+  page = option_menu("ciao", ["Biography", 
+                              "Ecology",
+                              "Data Science",
+                              "Photography", 
+                              "Music"], 
+#                      icons=["bi bi-info-circle","bi-calculator"],
+                     default_index=1, orientation="vertical",
+                     menu_icon="cast")
+  
