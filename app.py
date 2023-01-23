6ffc1a70781ebdd0afb0bec95104008407ac2807
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 19:53:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+page = option_menu(None,["Biography", 
+                          "Ecology",
+                          "Data Science",
+                          "Photography", 
+                          "Music"], 
+                 icons=["bi bi-info-lg","bi bi-tree-fill",
+                        "bi bi-bar-chart-fill","bi bi-camera2",
+                        "bi bi-file-earmark-music-fill"],
+                 default_index=0, orientation="orizontal",
+                 )
+
 st.title("My website")
 
-with st.sidebar:
-  page = option_menu(None,["Biography", 
-                              "Ecology",
-                              "Data Science",
-                              "Photography", 
-                              "Music"], 
-                     icons=["bi bi-info-lg","bi bi-tree-fill",
-                            "bi bi-bar-chart-fill","bi bi-camera2",
-                            "bi bi-file-earmark-music-fill"],
-                     default_index=0, orientation="vertical",
-                     )
+  
   
