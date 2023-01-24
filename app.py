04b05bc997b55f76120dcdd2e5d6dd3dd3d46333
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-24 07:41:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+import folium
+from folium.plugins import Draw, Fullscreen, LocateControl
+from streamlit_folium import st_folium
+
 st.set_page_config(
     page_title="Profile",
     page_icon="🧊",
@@ -9,7 +13,7 @@ st.set_page_config(
 )
 
 
-page = option_menu(None,["Biography", "---","Ecology","Data Science","Photography","Music"], 
+page = option_menu(None,["Biography", "Ecology","Data Science","Photography","Music"], 
                  icons=["bi bi-info-lg","bi bi-tree-fill",
                         "bi bi-bar-chart-fill","bi bi-camera2",
                         ],
@@ -17,6 +21,13 @@ page = option_menu(None,["Biography", "---","Ecology","Data Science","Photograph
                  )
 
 st.title("My website")
+
+if page == "Data Science":
+    m = folium.Map(location=[44.266308, 11.719301], zoom_start=3)
+    Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
+    Fullscreen().add_to(m)
+    LocateControl(auto_start=True).add_to(m)
+    output = st_folium(m, returned_objects=["all_drawings"], width=350, height=600)
 
   
   
