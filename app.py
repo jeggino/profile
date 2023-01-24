7349ad9fb7b4a9ec3e70ec7f89630fef1fdb2512
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-24 08:32:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,7 @@ if page == "Data Science":
                                  labels=['low','high'])
     
     df_2 = df_model.groupby('price_class').mean().round(2)
-    body = """
-    df_model['price_class'] = pd.cut(df_model.Price,
+    body = """df_model['price_class'] = pd.cut(df_model.Price,
                                     bins=[df_model["Price"].min(),
                                             df_model["Price"].mean(),
                                             df_model["Price"].max()],
