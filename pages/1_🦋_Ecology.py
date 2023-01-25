5cf776ef4d16d0b058d7ea451f4e0b2c2fa2f67d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-25 08:44:00 +0100

    Update 1_🦋_Ecology.py

--- pages/1_🦋_Ecology.py
+++ pages/1_🦋_Ecology.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 from streamlit_player import st_player
 
 import folium
-from folium.plugins import Draw, Fullscreen, LocateControl
+from folium import plugins
 from streamlit_folium import st_folium
 
 import pandas as pd
@@ -28,7 +28,6 @@ df_raw = pd.read_csv('pages/bird_migration (1).csv')
 df_raw["date_time"] = pd.to_datetime(df_raw["date_time"])
 df_1 = df_raw[['date_time','bird_name', 'latitude', 'longitude']]
 df_1['month'] = df_1.date_time.dt.month_name()
-st.dataframe(df_1)
 
 st.subheader("Calculate the distance covered per month")
 
@@ -39,10 +38,8 @@ geo_df = geo_df.groupby(['bird_name','month'])['geometry'].apply(lambda x:LineSt
 geo_df = gpd.GeoDataFrame(geo_df, geometry='geometry',crs={'init':'epsg:4326'}).reset_index()
 geo_df.to_crs(epsg=3310,inplace=True)
 geo_df['distance (Km)'] = round(geo_df.length / 1000)
-st.error("ERROR!!!!!!!!!!!!!!")
 
 source = pd.DataFrame(geo_df.drop(columns='geometry'))
-st.dataframe(source)
 
 altair_chart = alt.Chart(source).mark_bar(
 ).encode(
@@ -55,3 +52,89 @@ altair_chart = alt.Chart(source).mark_bar(
 )
 
 st.altair_chart(altair_chart, use_container_width=False, theme=None)
+
+
+st.subheader("Show the routes through the time")
+
+df_Eric = df_raw[df_raw.bird_name=='Eric'].reset_index(drop=True)
+df_Eric['date_time'] = df_Eric['date_time'].dt.date
+df_Nico = df_raw[df_raw.bird_name=='Nico'].reset_index(drop=True)
+df_Nico['date_time'] = df_Nico['date_time'].dt.date
+df_Sanne = df_raw[df_raw.bird_name=='Sanne'].reset_index(drop=True)
+df_Sanne['date_time'] = df_Sanne['date_time'].dt.date
+
+import folium
+from folium import plugins
+from branca.element import Figure
+
+
+
+# create the Heatmap
+fig=Figure(width=850,height=550)
+
+lat = df_raw.latitude.mean()
+long = df_raw.longitude.mean()
+
+m = folium.Map(location=[lat,long],zoom_start=3,tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr='Google_map')
+
+fig.add_child(m)
+
+
+lines = [
+    {
+        "coordinates": [[df_Eric.longitude[i],df_Eric.latitude[i]] for i in range(len(df_Eric))],
+        "dates": df_Eric.date_time.astype(str).tolist(),
+        "color": "red",
+    },
+    {
+        "coordinates": [[df_Nico.longitude[i],df_Nico.latitude[i]] for i in range(len(df_Nico))],
+        "dates": df_Nico.date_time.astype(str).tolist(),
+        "color": "blue",
+    },
+    {
+        "coordinates": [[df_Sanne.longitude[i],df_Sanne.latitude[i]] for i in range(len(df_Sanne))],
+        "dates": df_Sanne.date_time.astype(str).tolist(),
+        "color": "purple",
+    }
+]
+
+features = [
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": line["coordinates"],
+        },
+        "properties": {
+            "times": line["dates"],
+            "style": {
+                "color": line["color"],
+                "weight": line["weight"] if "weight" in line else 2,
+            },
+            "popup": "Eric" if line["color"]== "red"  else "Nico" if line["color"]== "blue" else 'Sanne',
+            "icon": "marker",
+            "iconstyle": {
+                "iconUrl":  "https://img.icons8.com/color/48/26e07f/seagull--v2.png" if line["color"]== "red"  else "https://img.icons8.com/fluency/48/000000/seagull.png",
+                "iconSize": [50,50],
+            }
+        },
+    }
+    for line in lines
+]
+
+plugins.TimestampedGeoJson(
+    {
+        "type": "FeatureCollection",
+        "features": features,
+    },
+    period="P1D",
+    add_last_point=True,
+    date_options='YYYY-MM-DD',
+    loop_button=True,
+    loop=False,).add_to(m)
+
+plugins.Fullscreen().add_to(m)
+
+st_folium(m)
+
+
