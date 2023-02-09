752665600d8275a1b3a424f74cadb7f9c008101a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-09 13:04:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -149,18 +149,14 @@ col3, col4, col5 = st.columns([2, 1, 1],gap="medium")
 
 
 with col1:
-    with st.container():
-        st.altair_chart(plot_1, use_container_width=False, theme="streamlit")
+    st.altair_chart(plot_1, use_container_width=False, theme="streamlit")
 with col2:
-    with st.container():
-        st.altair_chart(plot_2, use_container_width=False, theme="streamlit")
+    st.altair_chart(plot_2, use_container_width=False, theme="streamlit")
 with col3:
-    with st.container():
-        st.altair_chart(plot_3, use_container_width=False, theme="streamlit")
+    st.altair_chart(plot_3, use_container_width=False, theme="streamlit")
 with col4:
-    with st.container():
-        st.altair_chart(map_1, use_container_width=False, theme="streamlit") 
-with col1:
+    st.altair_chart(map_1, use_container_width=False, theme="streamlit") 
+with col5:
     st.dataframe(df,use_container_width=False)
 
 
