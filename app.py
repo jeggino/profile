a81a53e82b3ffdaa49e0616cc04088a5464ef14a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-09 13:41:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -200,6 +200,15 @@ gridOptions["animateRows"]=True
 gridOptions["groupDefaultExpanded"]= -1   # expand all
 gridOptions["getDataPath"]=JsCode("function(data){ return data.orgHierarchy.split('|'); }").js_code
 
+
+
+headers = ["symboling", "normalized_losses", "make", "fuel_type", "aspiration",
+           "num_doors", "body_style", "drive_wheels", "engine_location",
+           "wheel_base", "length", "width", "height", "curb_weight",
+           "engine_type", "num_cylinders", "engine_size", "fuel_system",
+           "bore", "stroke", "compression_ratio", "horsepower", "peak_rpm",
+           "city_mpg", "highway_mpg", "price"]
+
 df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data",
                  names=headers, 
                  na_values="?" 
